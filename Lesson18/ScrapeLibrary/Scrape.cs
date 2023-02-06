c2a1a75074a697b55a541ddfa40466d1cc7a9759
ALKHWARIZMI-PC\abelm <abelmanuellugtujr1996@gmail.com>
2023-02-06 19:30:33 +0800

    ✨ feat: 02-06-2023

---
+++ Lesson18/ScrapeLibrary/Scrape.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapeLibrary
+{
+    public class Scrape
+    {
+        public string ScrapeWebpage(string url)
+        {
+            return GetWebpage(url);
+        }
+        public string ScrapeWebpage(String url, string filepath)
+        {
+            string reply = GetWebpage(url);
+
+            System.IO.File.WriteAllText(@"C:\Users\abelm\Desktop\c-sharp-basics\WriteText.txt", reply);
+            return reply;
+        }
+
+        private string GetWebpage(string url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
+        }
+    }
+}
---
+++ MyCodeLibrary/MyCodeLibrary/Scrape.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MyCodeLibrary
+{
+    public class Scrape
+    {
+        public string ScrapeWebpage(string url)
+        {
+            return GetWebpage(url);
+        }
+        public string ScrapeWebpage(String url,string filepath)
+        {
+            string reply = GetWebpage(url);
+          
+            System.IO.File.WriteAllText(@"C:\Users\abelm\Desktop\c-sharp-basics\WriteText.txt", reply);
+            return reply;
+        }
+
+        private string GetWebpage(string url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
+        }
+    }
+}
