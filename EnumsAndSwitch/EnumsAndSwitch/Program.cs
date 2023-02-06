4b7452c78f849dc644ee8aa0465edc6913c5d1b1
ALKHWARIZMI-PC\abelm <abelmanuellugtujr1996@gmail.com>
2023-02-06 21:21:31 +0800

    ✨ feat: 02-06-2023

---
+++ EnumsAndSwitch/EnumsAndSwitch/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsAndSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Todo> todos = new List<Todo>()
+            {
+                new Todo {Description="Task 1",EstimatedHours=6,Status=Status.Completed},
+            };
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            PrintAssessment(todos);
+            Console.ReadLine();
+            
+          
+        }
+
+        private static void PrintAssessment(List<Todo> todos)
+        {
+            foreach (var todo in todos) 
+            {
+                switch (todo.Status)
+                {
+                    case Status.NotStarted:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    case Status.InProgress:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    case Status.OnHold:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        break;
+                    case Status.Completed:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        break;
+                    case Status.Deleted:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine(todo.Description);
+            }
+        }
+
+        class Todo
+        {
+            public string Description { get; set; }
+            public int EstimatedHours { get; set; }
+            public Status Status { get; set; }
+        }
+
+        enum Status
+        {
+            NotStarted,
+            InProgress,
+            OnHold,
+            Completed,
+            Deleted
+        }
+    }
+
+   
+
+}
---
+++ HandlingExceptions/HandlingExceptions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandlingExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = File.ReadAllText(@"C:\Users\abelm\Desktop\c-sharp-basics\Lesson22\Example.txt");
+                Console.WriteLine(content);
+                Console.ReadLine();
+            }
+            catch (FileNotFoundException) 
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the name of the file is named correctly!");
+            }
+            catch(DirectoryNotFoundException)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the directory C:\\Lesson22 exist!");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // Code to finalize
+                Console.WriteLine("Closing application now...");
+            }
+            Console.ReadLine();
+        }
+    }
+}
